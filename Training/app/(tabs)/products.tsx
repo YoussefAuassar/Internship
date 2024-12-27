002857f282d4f7269144fd09df9837a42d3db739
Youssef <youssef.auassar@student.ehb.be>
2024-12-27 18:18:32 +0100

    feat: add tabs for Home, Products, Lists, Scan & Win, and Profile

---
+++ Training/app/(tabs)/products.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ProductsScreen() {
+  return (
+    <View>
+      <Text>Products Screen</Text>
+    </View>
+  );
+}
+
+export default ProductsScreen;
---
+++ Training/app/(tabs)/scanWin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function scanWinScreen() {
+  return (
+    <View>
+      <Text>Scan & Win Screen</Text>
+    </View>
+  );
+}
+
+export default scanWinScreen;
